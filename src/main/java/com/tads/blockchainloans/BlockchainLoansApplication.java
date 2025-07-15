5f2874700dc6634df768500180ea702e4ddc919e
MiguelVaDu <miguelangel.valle@unmsm.edu.pe>
2025-07-15 15:37:01 -0500

    Creation Project

---
+++ src/main/java/com/tads/blockchainloans/BlockchainLoansApplication.java
@@ -0,0 +1,13 @@
+package com.tads.blockchainloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainLoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainLoansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tads/blockchainloans/BlockchainLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads.blockchainloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainLoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
